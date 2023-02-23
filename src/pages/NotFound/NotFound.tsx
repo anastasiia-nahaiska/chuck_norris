00c5a8a1d10837fb2d881f20a8020d8821788475
Anastasiia Nahaiska <nahaiskaanastasiia@gmail.com>
2023-02-23 12:53:24 +0200

    used navigate function for navigate to home from not found page

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { ErrorMeassage } from '../../components/ErrorMeassage';
 import { PrimaryButton } from '../../components/PrimaryButton';
@@ -6,21 +7,25 @@ import { page_not_found_message } from '../../constants/messages';
 
 import './NotFound.scss';
 
-export const NotFound: React.FC = () => (
-  <main className="not_found">
-    <div className="not_found__content">
-      <ErrorMeassage 
-        message={page_not_found_message} 
-        size={20} 
-        className="not_found__message"
-      />
+export const NotFound: React.FC = () => {
+  const navidate = useNavigate();
 
-      <a href="/">
+  
+  return (
+    <main className="not_found">
+      <div className="not_found__content">
+        <ErrorMeassage 
+          message={page_not_found_message} 
+          size={20} 
+          className="not_found__message"
+        />
+
         <PrimaryButton 
           text="Get back on track"
           className="not_found__button"
+          onCkick={() => navidate('/')} 
         />
-      </a>
-    </div>
-  </main>
-);
+      </div>
+    </main>
+  );
+};
